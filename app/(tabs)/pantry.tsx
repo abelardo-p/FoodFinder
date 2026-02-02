a29761108a4cf5bd3f5bb604cb63fdca387ea895
Abelardo Perez <abelardp@uci.edu>
2026-02-01 21:56:11 -0800

    added: TODO breakdown of components

---
+++ app/(tabs)/pantry.tsx
@@ -0,0 +1,14 @@
+import {ScrollView, View, Text } from 'react-native';
+import { ThemedView } from '@/components/themed-view';
+import { FlatList } from 'react-native-reanimated/lib/typescript/Animated';
+
+
+export default function Pantry() {
+  return (
+    <ThemedView>
+      {/* <FlatList>
+          
+      </FlatList> */}
+    </ThemedView>
+  );
+}
