c5510a4fcefe66de2af40bd71a7899749a4444b2
Reina El Khoury <relkhou1@uci.edu>
2026-02-07 19:23:33 -0800

    Set up basic frontend and backend

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import ItemCard from "@/components/ui/item-card";
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { Text, View } from "react-native";
+import Header from "../../components/Header";
+import Todos from "../../components/Todos";
 
 export default function Index() {
   return (
+	
     <View
       style={{
         flex: 1,
@@ -10,6 +14,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <ChakraProvider value={defaultSystem}>
+        <Header />
+        <Todos />
+      </ChakraProvider>
+
+
       <ItemCard head={<Text style={{fontWeight: '500'}}>Edit app/index.tsx to edit this screen.</Text>}>
       <View>
         <Text>Hidden Content</Text>
