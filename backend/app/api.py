28fe78e7368baa4538a261f758f52d7c2c7970b7
Reina El Khoury <relkhou1@uci.edu>
2026-02-13 21:24:02 -0800

    Committing changes

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
@@ -7,6 +9,8 @@ origins = [
     "http://localhost:8081",
     "localhost:8081"
 ]
+
+DATABASE_URL = "postgresql://user:password@localhost/dbname"
 
 
 app.add_middleware(
@@ -16,6 +20,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+DATABASE_URL = "postgresql://user:password@localhost/dbname"
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  
+    finally:
+        db.close() 
 
 todos = [
     {
@@ -28,7 +44,6 @@ todos = [
     },
 ]
 
-# DO THIS REINA. MAKE A SQLITE SCHEMA JUST FOR THE USER METADATA
 @app.post("/add-item", tags=["pantry"])
 async def add_item_to_pantry_list() -> dict:
 	# Check from postgres side to make sure it's a real item
