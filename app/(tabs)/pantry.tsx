b8b985a8391eff4e26a8047f7cf7fb4e2ea723af
Reina El Khoury <relkhou1@uci.edu>
2026-02-15 15:04:37 -0800

    Completed SQL Lite get for pantry. Need to do search and connect backend to frontend

--- app/(tabs)/pantry.tsx
+++ app/(tabs)/pantry.tsx
@@ -1,26 +1,29 @@
-import { Text, View, FlatList, LayoutAnimation, Pressable, StyleSheet} from "react-native";
 import ItemCard from "@/components/ui/item-card";
 import SearchBar from "@/components/ui/search-bar";
-import { useState } from "react";
+import * as dbFunctions from "@/src/schema";
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { FlatList, LayoutAnimation, Pressable, StyleSheet, Text, View } from "react-native";
 
 type Item = {
   id: string;
   name: string;
 }
-const data: Item[] = [
-  {id: '1', name: 'apple'},
-  {id: '2', name: 'banana'},
-  {id: '3', name: 'bread'},
-  {id: '4', name: 'potato'},
-  {id: '5', name: 'hamburger'},
-  {id: '6', name: 'soup'},
-  {id: '7', name: 'apple'},
-  {id: '8', name: 'banana'},
-  {id: '9', name: 'bread'},
-  {id: '10', name: 'potato'},
-  {id: '11', name: 'hamburger'},
-  {id: '12', name: 'soup'},
-];
+
+// const data: Item[] = [
+//   {id: '1', name: 'apple'},
+//   {id: '2', name: 'banana'},
+//   {id: '3', name: 'bread'},
+//   {id: '4', name: 'potato'},
+//   {id: '5', name: 'hamburger'},
+//   {id: '6', name: 'soup'},
+//   {id: '7', name: 'apple'},
+//   {id: '8', name: 'banana'},
+//   {id: '9', name: 'bread'},
+//   {id: '10', name: 'potato'},
+//   {id: '11', name: 'hamburger'},
+//   {id: '12', name: 'soup'},
+// ];
 
 const cardElement = (text: string) => {
   return (
@@ -40,9 +43,23 @@ const dataElement = (text: string) => {
 }
 
 export default function Index() {
+  const db = useSQLiteContext();
   const [isFocus, setFocus] = useState(false);
-  const [items, setItems] = useState<Item[]>(data);
+  const [items, setItems] = useState<Item[]>([]);
   const [activeId, setActiveId] = useState<string | null>(null);
+
+
+  useEffect(() => {
+
+	const fetchItems = async () => {
+		const foodItems = await dbFunctions.fetchItemsForPantry(db);
+		console.log(foodItems);
+		setItems((foodItems as Item[]) ?? []);
+	};
+
+	fetchItems();
+  }, []);
+
 
   const handleLongPress = (id: string) => {
     setActiveId(id);
@@ -53,8 +70,12 @@ export default function Index() {
       );
       setItems((prev) => prev.filter((item) => item.id !== id));
       setActiveId(null);
+	  dbFunctions.deleteItemFromDB(id, db);
     }, 300);
+
+	dbFunctions.printTable(db);
   };
+
   return (
     <View
       style={{
