65aba6b50ce27b8d82983f707031449e273f47c9
Reina El Khoury <relkhou1@uci.edu>
2026-02-15 17:05:52 -0800

    I don't understand

--- app/(tabs)/pantry.tsx
+++ app/(tabs)/pantry.tsx
@@ -1,34 +1,29 @@
-import { Text, View, FlatList, LayoutAnimation, Pressable, StyleSheet} from "react-native";
 import ItemCard from "@/components/ui/item-card";
 import SearchBar from "@/components/ui/search-bar";
-import { useState } from "react";
+import * as dbFunctions from "@/src/schema";
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { FlatList, LayoutAnimation, Pressable, StyleSheet, Text, View } from "react-native";
 
 type Item = {
   id: string;
   name: string;
 }
-const data: Item[] = [
-  { id: '1', name: 'Tomato' },
-  { id: '2', name: 'Onion' },
-  { id: '3', name: 'Garlic' },
-  { id: '4', name: 'Salt' },
-  { id: '5', name: 'Pepper' },
-  { id: '6', name: 'Olive Oil' },
-  { id: '7', name: 'Butter' },
-  { id: '8', name: 'Basil' },
-  { id: '9', name: 'Oregano' },
-  { id: '10', name: 'Parsley' },
-  { id: '11', name: 'Chicken' },
-  { id: '12', name: 'Beef' },
-  { id: '13', name: 'Pork' },
-  { id: '14', name: 'Carrot' },
-  { id: '15', name: 'Potato' },
-  { id: '16', name: 'Cheese' },
-  { id: '17', name: 'Milk' },
-  { id: '18', name: 'Egg' },
-  { id: '19', name: 'Flour' },
-  { id: '20', name: 'Sugar' },
-];
+
+// const data: Item[] = [
+//   {id: '1', name: 'apple'},
+//   {id: '2', name: 'banana'},
+//   {id: '3', name: 'bread'},
+//   {id: '4', name: 'potato'},
+//   {id: '5', name: 'hamburger'},
+//   {id: '6', name: 'soup'},
+//   {id: '7', name: 'apple'},
+//   {id: '8', name: 'banana'},
+//   {id: '9', name: 'bread'},
+//   {id: '10', name: 'potato'},
+//   {id: '11', name: 'hamburger'},
+//   {id: '12', name: 'soup'},
+// ];
 
 const cardElement = (text: string) => {
   return (
@@ -37,9 +32,9 @@ const cardElement = (text: string) => {
     </Text>
   )
 }
-const dataElement = (text: string, onPress: () => void) => {
+const dataElement = (text: string) => {
   return (
-    <Pressable onPress={onPress}style={{ justifyContent: 'center', alignItems: 'center', margin: 12}}>
+    <Pressable style={{ justifyContent: 'center', alignItems: 'center', margin: 12}}>
       <Text style={{fontSize: 16}}>
         {text}
       </Text>
@@ -48,21 +43,23 @@ const dataElement = (text: string, onPress: () => void) => {
 }
 
 export default function Index() {
+  const db = useSQLiteContext();
   const [isFocus, setFocus] = useState(false);
-  const [isGroupSelected, setGroupSelected] = useState(false);
-  const [isCatSelected, setCatSelected] = useState(false);
-  const [items, setItems] = useState<Item[]>(data);
+  const [items, setItems] = useState<Item[]>([]);
   const [activeId, setActiveId] = useState<string | null>(null);
 
 
-  const handleGroupPress = () => {
-    setGroupSelected(true);
-  }
-  const handleCatPress = () => {
-    setFocus(false);
-    setGroupSelected(false);
-    setCatSelected(false);
-  }
+  useEffect(() => {
+
+	const fetchItems = async () => {
+		const foodItems = await dbFunctions.fetchItemsForPantry(db);
+		console.log(foodItems);
+		setItems((foodItems as Item[]) ?? []);
+	};
+
+	fetchItems();
+  }, []);
+
 
   const handleLongPress = (id: string) => {
     setActiveId(id);
@@ -73,8 +70,12 @@ export default function Index() {
       );
       setItems((prev) => prev.filter((item) => item.id !== id));
       setActiveId(null);
+	  dbFunctions.deleteItemFromDB(id, db);
     }, 300);
+
+	dbFunctions.printTable(db);
   };
+
   return (
     <View
       style={{
@@ -88,49 +89,26 @@ export default function Index() {
         containerStyle={{}} 
         searchBarStyle={{ height: 60, width: 250, borderWidth: 3, borderRadius: 10, backgroundColor: 'snow'}}
         onChange={() => {}}
-        onFocus={() => {setFocus(true)}}
-        onBlur={() => {}}
+        onFocus={() => {setFocus(!isFocus)}}
+        onBlur={() => {setFocus(!isFocus)}}
       >
       </SearchBar>
-
       {isFocus ? (
-        <View style={{flexDirection: 'row'}}>
-          <FlatList
+        <FlatList
           data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item } ) => (
-            dataElement(item.name, handleGroupPress)
+            dataElement(item.name)
           )}
           style={{ shadowColor: '#000',
                   shadowOffset: { width: 1, height: 4 },
                   shadowOpacity: 0.2,
                   shadowRadius: 6,
-                  maxHeight: 450, width: 225, borderRadius: 11, borderWidth: 3, margin: 10, marginLeft: 3, marginRight: 3, padding: 10, backgroundColor: 'snow'}}
+                  maxHeight: 450, width: 225, borderRadius: 11, borderWidth: 3, margin: 10, padding: 10, backgroundColor: 'snow'}}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
         />
-
-        {isGroupSelected && !isCatSelected ? (
-          <FlatList
-            data={items}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item } ) => (
-              dataElement(item.name, handleCatPress)
-            )}
-            style={{ shadowColor: '#000',
-                    shadowOffset: { width: 1, height: 4 },
-                    shadowOpacity: 0.2,
-                    shadowRadius: 6,
-                    maxHeight: 450, width: 225, borderRadius: 11, borderWidth: 3, margin: 10, marginLeft: 3, marginRight: 3, padding: 10, backgroundColor: 'snow'}}
-            showsVerticalScrollIndicator={false}
-            ItemSeparatorComponent={() => <View style={styles.separator} />}
-          />
-        ) : (<View></View>)}
-        </View>
-
-
       ) : (<View style={{margin: 15}}></View>)}
-
       <FlatList
         data={items}
         keyExtractor={(item) => item.id}
